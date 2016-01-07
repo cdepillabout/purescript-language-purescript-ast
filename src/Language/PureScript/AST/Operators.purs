5e853534513b0abcb7c3c2fa39298a75c5b9a3d7
(cdep)illabout <cdep.illabout@gmail.com>
2016-01-07 12:44:28 +0900

    Creating AST.Operators file.

--- src/Language/PureScript/AST/Operators.purs
+++ src/Language/PureScript/AST/Operators.purs
@@ -1,16 +1,12 @@
 
 module Language.PureScript.Operators where
 
-import Prelude
-
 import Language.PureScript.Crash (internalError)
 
 
 type Precedence = Int
 
--- |
--- Associativity for infix operators
---
+-- | Associativity for infix operators
 data Associativity = Infixl | Infixr | Infix
 -- deriving (Show, Read, Eq, Ord, D.Data, D.Typeable)
 
@@ -30,3 +26,13 @@ readAssoc _ = internalError "readAssoc: no parse"
 
 -- instance A.FromJSON Associativity where
 --   parseJSON = fmap readAssoc . A.parseJSON
+
+-- | Fixity data for infix operators
+data Fixity = Fixity Associativity Precedence
+-- deriving (Show, Read, Eq, Ord, D.Data, D.Typeable)
+
+-- instance A.ToJSON Fixity where
+--   toJSON (Fixity associativity precedence) =
+--     A.object [ "associativity" .= associativity
+--              , "precedence" .= precedence
+--              ]
